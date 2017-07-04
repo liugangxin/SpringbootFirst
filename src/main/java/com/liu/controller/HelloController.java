5d93aab862ac4691223909b941a54b87f6d8a3f4
liugangxin <1181731633@qq.com>
2017-07-04 22:02:12 +0800

    删除webapp目录

--- src/main/java/com/liu/controller/HelloController.java
+++ src/main/java/com/liu/controller/HelloController.java
@@ -1,14 +1,22 @@
 package com.liu.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloController {
 
+	@RequestMapping("/")
+	String index() {
+		return "index";
+	}
+	
+	@ResponseBody
 	@RequestMapping("/hello/{Id}")
 	String home() {
 		return "Hello World!";
 	}
+	
 
 }
